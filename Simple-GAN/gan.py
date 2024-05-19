c12d2d7f35deefb5f6276cf93bd6100a2f8d63fd
Arya <83908779+aryad-16@users.noreply.github.com>
2024-05-19 10:45:00 +0530

    minor updates

--- Simple-GAN/gan.py
+++ Simple-GAN/gan.py
@@ -14,9 +14,9 @@ class Discriminator(nn.Module):
     def __init__(self, img_dim):
         super().__init__()
         self.disc = nn.Sequential(
-            nn.Linear(img_dim , 128),
+            nn.Linear(img_dim, 128),
             nn.LeakyReLU(0.1),
-            nn.Linear(128 , 1),
+            nn.Linear(128, 1),
             nn.Sigmoid()
         )
 
@@ -29,7 +29,7 @@ class Generator(nn.Module):
         self.gen = nn.Sequential(
             nn.Linear(z_dim, 256),
             nn.LeakyReLU(0.1),
-            nn.Linear(256 , img_dim),
+            nn.Linear(256, img_dim),
             nn.Tanh(),
         )
 
